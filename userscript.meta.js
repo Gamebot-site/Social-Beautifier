8ff1e8899da1c99537b5c491f092a1ff93a93dde
Gamebot-site <76865811+Gamebot-site@users.noreply.github.com>
2021-04-28 21:51:08 +0500

    Add files via upload

---
+++ userscript.meta.js
@@ -0,0 +1,7 @@
+ ==UserScript==
+ @name        Social Beautifier
+ @version     0.2
+ @author      BlackThunder#4007
+ @downloadURL https://raw.githubusercontent.com/Gamebot-site/Social-Beautifier/main/userscript.user.js
+ @updateURL   https://raw.githubusercontent.com/Gamebot-site/Social-Beautifier/main/userscript.meta.js
+ ==UserScript==
