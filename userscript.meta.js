dd92d5381c43d58093697c70c08f5550051d1b65
Gamebot-site <76865811+Gamebot-site@users.noreply.github.com>
2021-05-03 05:30:06 +0500

    Update userscript.meta.js

--- userscript.meta.js
+++ userscript.meta.js
@@ -1,6 +1,6 @@
  ==UserScript==
  @name        Social Beautifier
- @version     0.3
+ @version     0.4
  @author      BlackThunder#4007
  @downloadURL https://raw.githubusercontent.com/Gamebot-site/Social-Beautifier/main/userscript.user.js
  @updateURL   https://raw.githubusercontent.com/Gamebot-site/Social-Beautifier/main/userscript.meta.js
