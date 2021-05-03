4f49bb019e840d2f4b65724c21944a7341d6034f
Gamebot-site <76865811+Gamebot-site@users.noreply.github.com>
2021-05-03 05:51:59 +0500

    Delete userscript.meta.js

--- userscript.meta.js
+++
@@ -1,7 +0,0 @@
-//  ==UserScript==
-// @name        Social Beautifier
-// @version     0.5
-// @author      BlackThunder#4007
-// @downloadURL https://raw.githubusercontent.com/Gamebot-site/Social-Beautifier/main/userscript.user.js
-// @updateURL   https://raw.githubusercontent.com/Gamebot-site/Social-Beautifier/main/userscript.meta.js
-// ==UserScript==
